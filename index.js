bbce37326d5b7f37249d7a115190e0f436681f3b
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-06-10 17:07:46 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(a = 'roller-skate') {
+    let message = `This Saturday, I want to ${a}!`
+    return message;
+}
+
+function mondayWork(a = 'go to the office') {
+    let message = `This Monday, I will ${a}.`
+    return message;
+}
+
+
+
+function wrapAdjective(x) {
+    const innerFunction = function (y) {
+      return `You are ${x}${y}${x}!`;
+    };
+    return innerFunction
+  }
+  
+  wrapAdjective("*")("a hard worker");
